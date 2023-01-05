2dbd0533f796971b84bcf26c24d58c7481cff70a
JulianaGok <juliana.bitencourt@gok.digital>
2023-01-05 10:17:58 -0300

    melhorias

--- src/components/footer/styled.js
+++ src/components/footer/styled.js
@@ -1,10 +1,9 @@
 import styled from "styled-components";
 
 export const Container = styled.div`
-  /* position: absolute; */
+  position: relative;
   bottom: 0;
   width: 100%;
-  height: 2.5rem;  
 `;
 
 export const Logo = styled.img`
